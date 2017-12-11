9e22faeafe5f5872cfdb4c3544de67a8c265d0da
John Downs <john.downs@ordergroove.com>
2017-12-11 16:21:54 -0500

    Can read config file

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ extern crate reqwest;
 
 use git2::Repository;
 use git2::Remote;
-use std::io::Read;
+use std::env;
 use reqwest::header;
-use reqwest::Client;
+use std::path::PathBuf;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 
 fn get_url<'a>(origin: &'a Remote) -> Vec<&'a str> {
     let url = origin.url().map(|s| s.split(":"));
@@ -21,7 +24,7 @@ fn get_repo_details(url: Vec<&str>) -> (&str, &str) {
         .map(|v| v.collect())
         .expect("no project!");
     let project = project.get(0).expect("it's borked!");
-    
+
     return (owner, project);
 }
 
@@ -36,6 +39,19 @@ fn get_data(url: &str) -> Result<reqwest::Response, Box<std::error::Error>> {
     Ok(res)
 }
 
+fn read_ghreleaseauth() -> String {
+    let mut home_dir: PathBuf = env::home_dir().expect("Cannot find config file!");
+    home_dir.push(".ghreleaseauth");
+    let file = File::open(home_dir).expect("Unable to open file");
+    let mut reader = BufReader::new(file);
+    let mut buf = String::new();
+    let line = match reader.read_line(&mut buf) {
+        Ok(line) => line,
+        Err(e) => panic!("Failed to read file!")
+    };
+    return buf
+}
+
 fn main() {
     let repo = match Repository::open(".") {
         Ok(repo) => repo,
@@ -48,9 +64,12 @@ fn main() {
 
     let url = get_url(&origin);
     let (owner, project) = get_repo_details(url);
-   
+
     let target_url = format!("https://api.github.com/repos/{}/{}/releases", owner, project);
     let response = get_data(&target_url);
+
+    let filepath = read_ghreleaseauth();
+    println!("file {:?}", filepath);
 
     println!("Hello! {}, {}, {:?}", owner, project, response)
 }
